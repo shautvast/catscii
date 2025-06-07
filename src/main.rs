6ff4105aeac232d6cf222b570eedc0b94f49191b
Sander Hautvast <shautvast@gmail.com>
2025-06-07 08:39:12 +0200

    serve over http in color

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,74 @@
+// in `src/main.rs`
+
+use axum::{
+    Router,
+    body::BoxBody,
+    http::header,
+    response::{IntoResponse, Response},
+    routing::get,
+};
+use reqwest::StatusCode;
+use serde::Deserialize;
+
 #[tokio::main]
 async fn main() {
-   let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
+    let app = Router::new().route("/", get(root_get));
+
+    axum::Server::bind(&"0.0.0.0:5000".parse().unwrap())
+        .serve(app.into_make_service())
         .await
         .unwrap();
-    println!("Status: {}", res.status());
-    let body = res.text().await.unwrap();
-    println!("Body: {}", body);
+}
+
+async fn root_get() -> Response<BoxBody> {
+    match get_cat_ascii_art().await {
+        Ok(art) => (
+            StatusCode::OK,
+            //           was text/plain 👇
+            [(header::CONTENT_TYPE, "text/html; charset=utf-8")],
+            art,
+        )
+            .into_response(),
+        Err(e) => {
+            println!("Something went wrong: {e}");
+            (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+        }
+    }
+}
+async fn get_cat_ascii_art() -> color_eyre::Result<String> {
+    #[derive(Deserialize)]
+    struct CatImage {
+        url: String,
+    }
+
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let client = reqwest::Client::default();
+
+    let image = client
+        .get(api_url)
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Vec<CatImage>>()
+        .await?
+        .pop()
+        .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
+
+    let image_bytes = client
+        .get(image.url)
+        .send()
+        .await?
+        .error_for_status()?
+        .bytes()
+        .await?;
+
+    let image = image::load_from_memory(&image_bytes)?;
+    let ascii_art = artem::convert(
+        image,
+        artem::options::OptionBuilder::new()
+            .target(artem::options::TargetType::HtmlFile(true, true))
+            .build(),
+    );
+
+    Ok(ascii_art)
 }
